330e11c6e306625b5068b4624caf107bf0b8fcf5
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-07 18:28:27 +0300

    initial commit

---
+++ app/src/main/java/com/nicos/percentageswithanimationcompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nicos.percentageswithanimationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nicos.percentageswithanimationcompose.ui.theme.PercentagesWithAnimationComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PercentagesWithAnimationComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PercentagesWithAnimationComposeTheme {
+        Greeting("Android")
+    }
+}
