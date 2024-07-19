ba46550df4358fec67b42ba9dad7bf8b02c9fd74
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-20 01:34:22 +0300

    add library

--- PercentagesWithAnimationCompose/build.gradle.kts
+++ PercentagesWithAnimationCompose/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.ui.tooling)
 }
