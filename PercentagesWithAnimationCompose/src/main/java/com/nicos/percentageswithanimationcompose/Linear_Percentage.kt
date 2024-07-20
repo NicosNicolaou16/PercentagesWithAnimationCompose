f32ace2c30580bae1643faf0b0d1fa33b3230228
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-20 23:32:01 +0300

    handle the percentage for linear

--- PercentagesWithAnimationCompose/src/main/java/com/nicos/percentageswithanimationcompose/Linear_Percentage.kt
+++ PercentagesWithAnimationCompose/src/main/java/com/nicos/percentageswithanimationcompose/Linear_Percentage.kt
@@ -1,47 +1,77 @@
 package com.nicos.percentageswithanimationcompose
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp.Companion.Infinity
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
+
 
 @Composable
-fun LinearPercentage() {
+fun LinearPercentage(
+    currentValue: Float,
+    maximumValue: Float,
+) {
+    var progress by remember { mutableFloatStateOf(0F) }
+    val density = LocalDensity.current
+    var maxWidth by remember { mutableStateOf(0.dp) }
+    val progressAnimDuration = 1_500
+    val progressAnimation by animateFloatAsState(
+        targetValue = if (progress != Infinity.value) progress else 0F,
+        animationSpec = tween(durationMillis = progressAnimDuration, easing = FastOutSlowInEasing),
+        label = "",
+    )
+
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .clip(shape = RoundedCornerShape(21.dp))
+            .background(Color.Red)
+            .onGloballyPositioned {
+                maxWidth = with(density) {
+                    it.size.width.toDp()
+                }
+            },
         contentAlignment = Alignment.Center
     ) {
         Box(
             modifier = Modifier
                 .align(Alignment.CenterStart)
-                .width(100.dp)
+                .width(progressAnimation.dp)
                 .height(50.dp)
-                .background(Color.Red)
-
-        )
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterStart)
-                .width(50.dp)
-                .height(50.dp)
-                .zIndex(2f)
                 .background(Color.Blue)
         )
+    }
+    LaunchedEffect(Unit) {
+        progress = (currentValue * maxWidth.value.toInt()) / maximumValue
     }
 }
 
 @Preview
 @Composable
 fun LinearPercentagePreview() {
-    LinearPercentage()
+    LinearPercentage(currentValue = 50F, maximumValue = 100F)
 }
--- app/src/main/java/com/nicos/percentageswithanimationcomposeexample/MainActivity.kt
+++ app/src/main/java/com/nicos/percentageswithanimationcomposeexample/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.nicos.percentageswithanimationcompose.LinearPercentage
 import com.nicos.percentageswithanimationcomposeexample.ui.theme.PercentagesWithAnimationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +24,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PercentagesWithAnimationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) { innerPadding ->
+                    PercentagesWithAnimationCompose(innerPadding = innerPadding)
                 }
             }
         }
@@ -31,17 +36,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PercentagesWithAnimationComposeTheme {
-        Greeting("Android")
+fun PercentagesWithAnimationCompose(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LinearPercentage(currentValue = 50F, maximumValue = 100F)
     }
 }
