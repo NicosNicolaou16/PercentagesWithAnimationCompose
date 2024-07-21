70ac0017b3e3f01d94ae95aeef35596ea9e90e04
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-21 11:13:28 +0300

    update the documentation

--- PercentagesWithAnimationCompose/src/main/java/com/nicos/percentageswithanimationcompose/enums/LeftAndRightText.kt
+++ PercentagesWithAnimationCompose/src/main/java/com/nicos/percentageswithanimationcompose/enums/LeftAndRightText.kt
@@ -1,8 +1,26 @@
 package com.nicos.percentageswithanimationcompose.enums
 
+/**
+ * Enum for left and right text
+ * */
 enum class LeftAndRightText {
+    /**
+     * Show Left text Only
+     * */
     LEFT_ONLY,
+
+    /**
+     * Show Right text Only
+     * */
     RIGHT_ONLY,
+
+    /**
+     * Show Left and Right text
+     * */
     BOTH,
+
+    /**
+     * Show Nothing
+     * */
     NONE
 }
