9dc30bca28c7b7529deef3c7dbba84686cdd5854
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-04-13 19:29:11 +0300

    update

--- PercentagesWithAnimationCompose/build.gradle.kts
+++ PercentagesWithAnimationCompose/build.gradle.kts
@@ -73,7 +73,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.NicosNicolaou16"
                 artifactId = "PercentagesWithAnimationCompose"
-                version = "1.3.0"
+                version = "1.2.4"
                 from(components["release"])
             }
         }
